9e98ed6aee3c7da9a95be44df123764b995ee50b
marcosismael21 <marcosismaelrodriguez949@gmail.com>
2025-02-06 00:45:41 -0600

    se arreglo el auth

--- src/repositories/rolRepositorry.js
+++ src/repositories/rolRepositorry.js
@@ -33,7 +33,7 @@ const createRol = async (data) => {
         })
 
         if (existeRol) {
-            return ResponseHandler.error('El rol ya existe en el sistema', 400)
+            return ResponseHandler.error('El rol ya existe en el sistema', 404)
         }
 
         const rol = await Rol.create(data)
--- src/routes/rol.routes.js
+++ src/routes/rol.routes.js
@@ -4,9 +4,9 @@ const { verifyToken } = require('../middleware/index')
 const router = express.Router()
 const { variasValidationRules, validate } = require('../middleware/validaciones/validacionesVarias')
 
-router.get('/', rolController.getAllRol)
+router.get('/', verifyToken, rolController.getAllRol)
 router.get('/:id', verifyToken, rolController.getRolById)
-router.post('/', [variasValidationRules(), validate], rolController.createRol)
+router.post('/', [verifyToken, variasValidationRules(), validate], rolController.createRol)
 router.put('/:id', [verifyToken, variasValidationRules(), validate], rolController.updateRol)
 router.delete('/:id', verifyToken, rolController.deleteRol)
 
