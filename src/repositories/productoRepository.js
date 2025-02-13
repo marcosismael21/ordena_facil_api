19d567880763bc70f49becacfaa35902c03d4851
marcosismael21 <marcosismaelrodriguez949@gmail.com>
2025-02-12 23:17:21 -0600

    se modifico el metodo de getAllProductos para mostrar las relaciones cons u descripcion

--- src/repositories/productoRepository.js
+++ src/repositories/productoRepository.js
@@ -2,9 +2,33 @@ const ResponseHandler = require('../utils/responseHandler')
 const db = require('../models')
 const Producto = db.Producto
 
+const { sequelize } = require("../models");
+const { QueryTypes, Transaction, } = require('sequelize');
+
 const getAllProducto = async () => {
     try {
-        const producto = await Producto.findAll()
+
+        const sql = `SELECT 
+                        pd.id,
+                        pd.descripcion,
+                        pd.proveedor_id,
+                        pr.nombre as nombreProveedor,
+                        pd.tipo_medida_id,
+                        tm.descripcion as tipoMedida,
+                        pd.categoria_id,
+                        ct.descripcion as categoria,
+                        pd.cantidad,
+                        pd.codigo,
+                        pd.estado
+                    FROM productos AS pd    
+                    left join proveedors as pr on pr.id = pd.proveedor_id
+                    left join tipomedidas as tm on tm.id = pd.tipo_medida_id
+                    left join categorias as ct on ct.id = pd.categoria_id`
+
+        const producto = await sequelize.query(sql, {
+            type: QueryTypes.SELECT
+        })
+        
         return ResponseHandler.success(producto)
     } catch (error) {
         throw error
@@ -31,6 +55,16 @@ const createProducto = async (data) => {
                 descripcion: data.descripcion
             }
         })
+
+        const existeProductoCodigo = await Producto.findOne({
+            where: {
+                codigo: data.codigo
+            }
+        })
+
+        if (existeProductoCodigo) {
+            return ResponseHandler.error('El código del producto ya existe en el sistema')
+        }
 
         if (existeProducto) {
             return ResponseHandler.error('El producto ya existe en el sistema')
