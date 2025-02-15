aa2dcb7250a5404654ef9910d8e05fc91c12a225
marcosismael21 <marcosismaelrodriguez949@gmail.com>
2025-02-14 23:22:30 -0600

    modificando los metodos de crear y editar

--- src/controllers/platilloController.js
+++ src/controllers/platilloController.js
@@ -1,4 +1,6 @@
 const platilloService = require('../services/platilloService')
+const uploadService = require('../services/uploadService')
+const { upload } = require('../middleware/uploadMiddleware')
 
 const getAllPlatillo = async (req, res, next) => {
     try {
@@ -20,29 +22,52 @@ const getPlatilloById = async (req, res, next) => {
 }
 
 const createPlatillo = async (req, res, next) => {
-    const {
-        nombre,
-        descripcion,
-        tipoplatilloId,
-        precio,
-        imageUrl,
-        productoIds,
-        cantidad,
-        estado
-    } = req.body
-
-    const data = {
-        nombre,
-        descripcion,
-        tipoplatilloId,
-        precio,
-        imageUrl,
-        productoIds,
-        cantidad,
-        estado
-    }
-
     try {
+
+        if (!req.file) {
+            return res.status(400).json({
+                success: false,
+                message: 'Debe proporcionar una imagen para el platillo'
+            })
+        }
+
+        const uploadResult = await uploadService.uploadFile(req.file)
+        if (!uploadResult.success) {
+            return res.status(400).json(uploadResult)
+        }
+
+        let productoIds = []
+        let cantidad = []
+
+        try {
+            productoIds = JSON.parse(req.body.productoIds)
+            cantidad = JSON.parse(req.body.cantidad)
+        } catch (error) {
+            return res.status(400).json({
+                success: false,
+                message: 'El formato de productoIds o cantidad es inválido. Debe ser un array en formato JSON'
+            })
+        }
+
+        const {
+            nombre,
+            descripcion,
+            tipoplatilloId,
+            precio,
+            estado
+        } = req.body
+
+        const data = {
+            nombre,
+            descripcion,
+            tipoplatilloId,
+            precio,
+            imageUrl: uploadResult.data.url,
+            productoIds,
+            cantidad,
+            estado
+        }
+
         const platillo = await platilloService.createPlatillo(data)
         return res.status(200).json(platillo)
     } catch (error) {
@@ -52,30 +77,49 @@ const createPlatillo = async (req, res, next) => {
 
 const updatePlatillo = async (req, res, next) => {
     const id = req.params.id
-
-    const {
-        nombre,
-        descripcion,
-        tipoplatilloId,
-        precio,
-        imageUrl,
-        productoIds,
-        cantidad,
-        estado
-    } = req.body
-
-    const data = {
-        nombre,
-        descripcion,
-        tipoplatilloId,
-        precio,
-        imageUrl,
-        productoIds,
-        cantidad,
-        estado
-    }
-
     try {
+        let imageUrl = req.body.imageUrl
+
+        if (req.file) {
+            const uploadResult = await uploadService.uploadFile(req.file);
+            if (!uploadResult.success) {
+                return res.status(400).json(uploadResult);
+            }
+            imageUrl = uploadResult.data.url;
+        }
+
+        let productoIds = []
+        let cantidad = []
+
+        try {
+            productoIds = JSON.parse(req.body.productoIds)
+            cantidad = JSON.parse(req.body.cantidad)
+        } catch (error) {
+            return res.status(400).json({
+                success: false,
+                message: 'El formato de productoIds o cantidad es inválido. Debe ser un array en formato JSON'
+            })
+        }
+
+        const {
+            nombre,
+            descripcion,
+            tipoplatilloId,
+            precio,
+            estado
+        } = req.body
+
+        const data = {
+            nombre,
+            descripcion,
+            tipoplatilloId,
+            precio,
+            imageUrl,
+            productoIds,
+            cantidad,
+            estado
+        }
+
         const platillo = await platilloService.updatePlatillo(data, id)
         return res.status(200).json(platillo)
     } catch (error) {
--- src/routes/platillo.routes.js
+++ src/routes/platillo.routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const platilloController = require('../controllers/platilloController')
 const { verifyToken } = require('../middleware/index')
+const { upload } = require('../middleware/uploadMiddleware')
 const router = express.Router()
 const { ValidationRules, validate } = require('../middleware/validaciones/platilloValidation')
 const validateApiKey = require('../middleware/apiKeyMiddleware')
@@ -11,8 +12,8 @@ router.use(validatePlatform)
 
 router.get('/', verifyToken, platilloController.getAllPlatillo)
 router.get('/:id', verifyToken, platilloController.getPlatilloById)
-router.post('/', [verifyToken, ValidationRules(), validate], platilloController.createPlatillo)
-router.put('/:id', [verifyToken, ValidationRules(), validate], platilloController.updatePlatillo)
+router.post('/', verifyToken, upload.single('image'), platilloController.createPlatillo)
+router.put('/:id', verifyToken, upload.single('image'), platilloController.updatePlatillo)
 router.delete('/:id', verifyToken, platilloController.deletePlatillo)
 
 module.exports = router
